420a0dde7d703b2f7e316b6062d0000531dd0620
JaviPedrote <pedrote3.1416gmail.com>
2025-01-02 10:04:03 +0100

    responsive

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -9,7 +9,7 @@ function Home() {
       id="home"
       className="h-[100vh] lg:h-[80vh] mt-32 mb-5 flex flex-col-reverse lg:flex-row justify-center gap-6 lg:gap-0 items-center bg-gray-900"
     >
-      <div className="text-center lg:text-left lg:w-1/3 flex-1 space-y-6 mb-14 lg:mb-0 lg:pl-32 mx-10 lg:-mr-10 ">
+      <div className="text-center lg:text-left lg:w-1/3 flex-1 space-y-6 mb-14 lg:mb-0 lg:pl-32 mx-10 lg:mr-10">
         <p className="text-lg text-green-500">👋 Hola, soy <span className="text-2xl font-bold">Javier Pedrote Molina</span></p>
         <h1 className="text-4xl lg:text-6xl font-bold text-green-500">
           Frontend Developer.
@@ -28,11 +28,11 @@ function Home() {
         Con <span className="text-green-500">2 años</span> de experiencia académica, me especializo en desarrollar interfaces modernas, funcionales y responsivas.
         </p>
       </div>
-      <div className="lg:w-[50vw] flex-1  flex justify-center">
+      <div className="w-full lg:w-[50vw] flex-1 flex justify-center">
         <img
           src={Imagen}
           alt="Programador trabajando"
-          className="w-[70vw] h-[70vw] lg:w-[35vw] lg:h-[35vw] max-h-[500px] max-w-[500px] rounded-[2%] imagen-difuminada"
+          className=" object-contain w-[70vw] h-[70vw] lg:w-[35vw] lg:h-[35vw] max-h-[500px] max-w-[500px] rounded-[2%] imagen-difuminada"
         />
       </div>
     </section>
