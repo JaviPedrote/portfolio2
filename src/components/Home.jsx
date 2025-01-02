0127a6b8d85bf3f991a7390fe6d35721b93d954b
JaviPedrote <pedrote3.1416gmail.com>
2025-01-02 09:30:54 +0100

    Cambio Imagen

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,21 +1,23 @@
 
-import ProgrammerImage from "../assets/programmer.png"; // Cambia esto por el nombre de tu imagen.
+import Imagen from "../assets/imagen3.webp"; 
+
+
 
 function Home() {
   return (
     <section
       id="home"
-      className="h-full mt-20 mb-5 flex flex-col lg:flex-row justify-center items-center bg-gray-900"
+      className="h-[100vh] lg:h-[80vh] mt-32 mb-5 flex flex-col-reverse lg:flex-row justify-center gap-6 lg:gap-0 items-center bg-gray-900"
     >
-      <div className="text-center lg:text-left lg:w-1/3 space-y-6 mb-14 lg:mb-0 mx-10 lg:mx-0">
+      <div className="text-center lg:text-left lg:w-1/3 flex-1 space-y-6 mb-14 lg:mb-0 lg:pl-32 mx-10 lg:-mr-10 ">
         <p className="text-lg text-green-500">👋 Hola, soy <span className="text-2xl font-bold">Javier Pedrote Molina</span></p>
-        <h1 className="text-4xl lg:text-5xl font-bold text-green-500">
+        <h1 className="text-4xl lg:text-6xl font-bold text-green-500">
           Frontend Developer.
         </h1>
     
         
-        <h2 className="text-3xl font-semibold text-white text-left">Habilidades principales:</h2>
-        <ul className="list-disc list-inside text-gray-400 text-left ml-10">
+        <h2 className="text-4xl font-semibold text-white text-left">Habilidades principales:</h2>
+        <ul className="list-disc  text-gray-400 text-lg text-left ml-10">
           <li><span className="text-green-500 font-medium">HTML5</span> y <span className="text-green-500 font-medium">CSS3</span>: Fundamentos sólidos en maquetación web.</li>
           <li><span className="text-green-500 font-medium">Tailwind CSS</span>: Diseño eficiente y componentes reutilizables.</li>
           <li><span className="text-green-500 font-medium">JavaScript</span> (ES6+): Desarrollo interactivo y dinámico.</li>
@@ -26,11 +28,11 @@ function Home() {
         Con <span className="text-green-500">2 años</span> de experiencia académica, me especializo en desarrollar interfaces modernas, funcionales y responsivas.
         </p>
       </div>
-      <div className="lg:w-1/2 flex justify-center rounded-full">
+      <div className="lg:w-[50vw] flex-1  flex justify-center">
         <img
-          src={ProgrammerImage}
+          src={Imagen}
           alt="Programador trabajando"
-          className="w-[40%] lg:w-[65%] rounded-[2%]"
+          className="w-[70vw] h-[70vw] lg:w-[35vw] lg:h-[35vw] max-h-[500px] max-w-[500px] rounded-[2%] imagen-difuminada"
         />
       </div>
     </section>
