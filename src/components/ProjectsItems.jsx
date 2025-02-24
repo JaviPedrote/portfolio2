e1124cb5cc58e6042b3348ff61daf2df775426a7
JaviPedrote <pedrote3.1416gmail.com>
2025-02-24 09:33:25 +0100

    sliderProjects

--- src/components/ProjectsItems.jsx
+++ src/components/ProjectsItems.jsx
@@ -7,6 +7,7 @@ export const ProjectsItems = () => {
     const settings = {
         dots: true,
         infinite: true,
+        arrows:false,
         slidesToShow: 3,
         slidesToScroll: 1,
         autoplay: true,
@@ -15,16 +16,16 @@ export const ProjectsItems = () => {
     };
     return (
         <div className="w-full flex justify-center items-center mt-24 h-24">
-            <div className="slider-container w-[60vw] lg:w-[600px] rounded-lg h-full">
+            <div className="slider-container w-[80vw] lg:w-[600px] rounded-lg h-full">
                 <Slider {...settings}>
-                    <div className="px-1 cursor-pointer focus:outline-none">
-                        <a href="https://simulacros.vercel.app/" className=""><img src={img1} className="rounded-lg h-[10vh] select-none" alt="imagen" /></a>
+                    <div className="lg:px-1 cursor-pointer focus:outline-none">
+                        <a href="https://simulacros.vercel.app/"><img src={img1} className="rounded-lg h-[10vh]" alt="imagen" /></a>
                     </div>
-                    <div className="px-1 cursor-pointer focus:outline-none">
-                        <a href="https://simulacros.vercel.app/" className=""><img src={img1} className="rounded-lg h-[10vh] select-none" alt="imagen" /></a>
+                    <div className="lg:px-1 cursor-pointer focus:outline-none">
+                        <a href="https://simulacros.vercel.app/"><img src={img1} className="rounded-lg h-[10vh]" alt="imagen" /></a>
                     </div>
-                    <div className="px-1 cursor-pointer focus:outline-none">
-                        <a href="https://simulacros.vercel.app/" className=""><img src={img1} className="rounded-lg h-[10vh] select-none" alt="imagen" /></a>
+                    <div className="lg:px-1 cursor-pointer focus:outline-none">
+                        <a href="https://simulacros.vercel.app/" ><img src={img1} className="rounded-lg h-[10vh]" alt="imagen" /></a>
                     </div>
                 </Slider>
             </div>
